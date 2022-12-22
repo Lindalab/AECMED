48bc34bda3a3bb84a759033e1f5eb3cba7931514
Lindalab <linda.arthur@ashesi.edu.gh>
2022-12-22 08:59:29 +0000

    Initials

---
+++ aecdb.sql
@@ -0,0 +1,559 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 22, 2022 at 09:56 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `aecdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `business`
+--
+
+CREATE TABLE `business` (
+  `business_id` int(11) NOT NULL,
+  `date_started` date NOT NULL,
+  `busines_name` varchar(50) DEFAULT NULL,
+  `business_logo` varchar(100) NOT NULL,
+  `business_location` varchar(50) DEFAULT NULL,
+  `business_contact` int(15) NOT NULL,
+  `business_email` varchar(30) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  `business_type` int(3) DEFAULT 0,
+  `business_description` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `business_details`
+--
+
+CREATE TABLE `business_details` (
+  `business_id` int(11) NOT NULL,
+  `formalised_structure` varchar(100) NOT NULL,
+  `sector_of_business` varchar(60) NOT NULL,
+  `sdg_alignment` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `business_grants`
+--
+
+CREATE TABLE `business_grants` (
+  `grant_id` int(11) NOT NULL,
+  `business_id` int(11) NOT NULL,
+  `amount` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `business_revenue`
+--
+
+CREATE TABLE `business_revenue` (
+  `business_id` int(11) NOT NULL,
+  `revenue_amount` double DEFAULT NULL,
+  `revenue_year` year(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `course_id` int(11) NOT NULL,
+  `course_name` varchar(50) NOT NULL,
+  `date_started` date NOT NULL,
+  `course_description` varchar(300) NOT NULL,
+  `course_status` int(3) NOT NULL,
+  `department_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_project`
+--
+
+CREATE TABLE `course_project` (
+  `course_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_student`
+--
+
+CREATE TABLE `course_student` (
+  `course_id` int(11) NOT NULL,
+  `numberoffemaleStudent` int(11) NOT NULL,
+  `numberofmaleStudent` int(11) NOT NULL,
+  `course_year` year(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `department_id` int(11) NOT NULL,
+  `deparment_name` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `event_id` int(11) NOT NULL,
+  `event_name` varchar(60) NOT NULL,
+  `date_organized` date NOT NULL,
+  `target_group` varchar(20) NOT NULL,
+  `event_description` varchar(300) NOT NULL,
+  `male_attendance` int(11) NOT NULL,
+  `female_attendace` int(11) NOT NULL,
+  `event_type` varchar(20) NOT NULL,
+  `department_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events_grants`
+--
+
+CREATE TABLE `events_grants` (
+  `grant_id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `amount` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grants`
+--
+
+CREATE TABLE `grants` (
+  `grant_id` int(11) NOT NULL,
+  `grant_name` varchar(50) NOT NULL,
+  `grant_type` int(3) NOT NULL,
+  `date_received` date NOT NULL,
+  `grant_description` varchar(600) DEFAULT NULL,
+  `grant_company_image` varchar(100) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  `amount` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `module`
+--
+
+CREATE TABLE `module` (
+  `module_id` int(11) NOT NULL,
+  `module_name` varchar(50) NOT NULL,
+  `module_description` varchar(100) NOT NULL,
+  `department_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+CREATE TABLE `project` (
+  `project_id` int(11) NOT NULL,
+  `project_name` varchar(50) NOT NULL,
+  `project_description` varchar(300) NOT NULL,
+  `project_status` int(3) NOT NULL,
+  `date_started` date NOT NULL,
+  `sdg_goals` varchar(15) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  `sector` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_grants`
+--
+
+CREATE TABLE `project_grants` (
+  `grant_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `amount` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stakeholder`
+--
+
+CREATE TABLE `stakeholder` (
+  `stakeholder_id` int(11) NOT NULL,
+  `first_name` varchar(20) DEFAULT NULL,
+  `last_name` varchar(20) DEFAULT NULL,
+  `stakeholderRole` int(11) NOT NULL,
+  `gender` int(3) NOT NULL DEFAULT 0,
+  `email` varchar(30) NOT NULL,
+  `phone_number` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stakeholder_business`
+--
+
+CREATE TABLE `stakeholder_business` (
+  `stakeholder_id` int(11) NOT NULL,
+  `business_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stakeholder_modules`
+--
+
+CREATE TABLE `stakeholder_modules` (
+  `stakeholder_id` int(11) NOT NULL,
+  `module_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stakeholder_project`
+--
+
+CREATE TABLE `stakeholder_project` (
+  `stakeholder_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `business`
+--
+ALTER TABLE `business`
+  ADD PRIMARY KEY (`business_id`),
+  ADD UNIQUE KEY `business_email` (`business_email`),
+  ADD UNIQUE KEY `business_contact` (`business_contact`),
+  ADD KEY `department_id` (`department_id`);
+
+--
+-- Indexes for table `business_details`
+--
+ALTER TABLE `business_details`
+  ADD KEY `business_id` (`business_id`);
+
+--
+-- Indexes for table `business_grants`
+--
+ALTER TABLE `business_grants`
+  ADD KEY `business_id` (`business_id`),
+  ADD KEY `grant_id` (`grant_id`);
+
+--
+-- Indexes for table `business_revenue`
+--
+ALTER TABLE `business_revenue`
+  ADD KEY `business_id` (`business_id`);
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`course_id`),
+  ADD KEY `department_id` (`department_id`);
+
+--
+-- Indexes for table `course_project`
+--
+ALTER TABLE `course_project`
+  ADD KEY `course_id` (`course_id`),
+  ADD KEY `project_id` (`project_id`);
+
+--
+-- Indexes for table `course_student`
+--
+ALTER TABLE `course_student`
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`department_id`);
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`event_id`),
+  ADD KEY `department_id` (`department_id`);
+
+--
+-- Indexes for table `events_grants`
+--
+ALTER TABLE `events_grants`
+  ADD KEY `event_id` (`event_id`),
+  ADD KEY `grant_id` (`grant_id`);
+
+--
+-- Indexes for table `grants`
+--
+ALTER TABLE `grants`
+  ADD PRIMARY KEY (`grant_id`),
+  ADD KEY `department_id` (`department_id`);
+
+--
+-- Indexes for table `module`
+--
+ALTER TABLE `module`
+  ADD PRIMARY KEY (`module_id`),
+  ADD KEY `department_id` (`department_id`);
+
+--
+-- Indexes for table `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`project_id`),
+  ADD KEY `department_id` (`department_id`);
+
+--
+-- Indexes for table `project_grants`
+--
+ALTER TABLE `project_grants`
+  ADD KEY `grant_id` (`grant_id`),
+  ADD KEY `project_id` (`project_id`);
+
+--
+-- Indexes for table `stakeholder`
+--
+ALTER TABLE `stakeholder`
+  ADD PRIMARY KEY (`stakeholder_id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `phone_number` (`phone_number`);
+
+--
+-- Indexes for table `stakeholder_business`
+--
+ALTER TABLE `stakeholder_business`
+  ADD KEY `business_id` (`business_id`),
+  ADD KEY `stakeholder_id` (`stakeholder_id`);
+
+--
+-- Indexes for table `stakeholder_modules`
+--
+ALTER TABLE `stakeholder_modules`
+  ADD KEY `module_id` (`module_id`),
+  ADD KEY `stakeholder_id` (`stakeholder_id`);
+
+--
+-- Indexes for table `stakeholder_project`
+--
+ALTER TABLE `stakeholder_project`
+  ADD KEY `project_id` (`project_id`),
+  ADD KEY `stakeholder_id` (`stakeholder_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `business`
+--
+ALTER TABLE `business`
+  MODIFY `business_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `grants`
+--
+ALTER TABLE `grants`
+  MODIFY `grant_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `module`
+--
+ALTER TABLE `module`
+  MODIFY `module_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `project`
+--
+ALTER TABLE `project`
+  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `stakeholder`
+--
+ALTER TABLE `stakeholder`
+  MODIFY `stakeholder_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `business`
+--
+ALTER TABLE `business`
+  ADD CONSTRAINT `business_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `business_details`
+--
+ALTER TABLE `business_details`
+  ADD CONSTRAINT `business_details_ibfk_1` FOREIGN KEY (`business_id`) REFERENCES `business` (`business_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `business_grants`
+--
+ALTER TABLE `business_grants`
+  ADD CONSTRAINT `business_grants_ibfk_1` FOREIGN KEY (`business_id`) REFERENCES `business` (`business_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `business_grants_ibfk_2` FOREIGN KEY (`grant_id`) REFERENCES `grants` (`grant_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `business_revenue`
+--
+ALTER TABLE `business_revenue`
+  ADD CONSTRAINT `business_revenue_ibfk_1` FOREIGN KEY (`business_id`) REFERENCES `business` (`business_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `course`
+--
+ALTER TABLE `course`
+  ADD CONSTRAINT `course_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `course_project`
+--
+ALTER TABLE `course_project`
+  ADD CONSTRAINT `course_project_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `course_project_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `course_student`
+--
+ALTER TABLE `course_student`
+  ADD CONSTRAINT `course_student_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `events_grants`
+--
+ALTER TABLE `events_grants`
+  ADD CONSTRAINT `events_grants_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`event_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `events_grants_ibfk_2` FOREIGN KEY (`grant_id`) REFERENCES `grants` (`grant_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `grants`
+--
+ALTER TABLE `grants`
+  ADD CONSTRAINT `grants_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `module`
+--
+ALTER TABLE `module`
+  ADD CONSTRAINT `module_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `project`
+--
+ALTER TABLE `project`
+  ADD CONSTRAINT `project_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `project_grants`
+--
+ALTER TABLE `project_grants`
+  ADD CONSTRAINT `project_grants_ibfk_1` FOREIGN KEY (`grant_id`) REFERENCES `grants` (`grant_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `project_grants_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `stakeholder_business`
+--
+ALTER TABLE `stakeholder_business`
+  ADD CONSTRAINT `stakeholder_business_ibfk_1` FOREIGN KEY (`business_id`) REFERENCES `business` (`business_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `stakeholder_business_ibfk_2` FOREIGN KEY (`stakeholder_id`) REFERENCES `stakeholder` (`stakeholder_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `stakeholder_modules`
+--
+ALTER TABLE `stakeholder_modules`
+  ADD CONSTRAINT `stakeholder_modules_ibfk_1` FOREIGN KEY (`module_id`) REFERENCES `module` (`module_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `stakeholder_modules_ibfk_2` FOREIGN KEY (`stakeholder_id`) REFERENCES `stakeholder` (`stakeholder_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `stakeholder_project`
+--
+ALTER TABLE `stakeholder_project`
+  ADD CONSTRAINT `stakeholder_project_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `stakeholder_project_ibfk_2` FOREIGN KEY (`stakeholder_id`) REFERENCES `stakeholder` (`stakeholder_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
