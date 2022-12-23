62178885aabfa0e21e13d9087e63a6df1c4bebd4
Lindalab <linda.arthur@ashesi.edu.gh>
2022-12-23 14:16:09 +0000

    MMy

--- classes/department_class.php
+++ classes/department_class.php
@@ -4,27 +4,27 @@
     class Department extends db_connection{
 
         function create_department($department_name){
-            $sql = "";
+            $sql = "INSERT INTO `department`(`deparment_name`) VALUES ('$department_name')";
             return $this->db_query($sql);
         }
 
         function update_department($department_id, $department_name){
-            $sql = "";
+            $sql = "UPDATE `department` SET `deparment_name`=' $department_name' WHERE `department_id`='$department_id' ";
             return $this->db_query($sql);
         }
 
         function delete_department($department_id){
-            $sql = "";
+            $sql = "DELETE FROM `department` WHERE `department_id`='$department_id'";
             return $this->db_query($sql);
         }
 
         function select_all_department(){
-            $sql = "";
+            $sql = "SELECT * FROM `department` ";
             return $this->db_fetch_all($sql);
         }
 
         function select_one_department($department_id){
-            $sql = "";
+            $sql = "SELECT * FROM `department` WHERE `department_id`='$department_id'";
             return $this->db_fetch_one($sql);
         }
     }
--- classes/module_class.php
+++ classes/module_class.php
@@ -5,27 +5,27 @@
     class Module extends db_connection{
 
         function create_module($module_name, $desc, $department_id){
-            $sql = "";
+            $sql = "INSERT INTO `module`(`module_name`, `module_description`, `department_id`) VALUES ('$module_name','$desc','$department_id')";
             return $this->db_query($sql);
         }
 
         function update_module($module_id, $module_name, $desc, $department_id){
-            $sql = "";
+            $sql = "UPDATE `module` SET `module_name`='$module_name',`module_description`='$desc',`department_id`='$department_id' WHERE `module_id`='$module_id'";
             return $this->db_query($sql);
         }
 
         function delete_module($module_id){
-            $sql = "";
+            $sql = "DELETE FROM `module` WHERE `module_id`='$module_id' ";
             return $this->db_query($sql);
         }
 
         function select_all_module(){
-            $sql = "";
+            $sql = "SELECT * FROM `module` ";
             return $this->db_fetch_all($sql);
         }
 
         function select_one_module($module_id){
-            $sql = "";
+            $sql = "SELECT * FROM `module` WHERE `module_id`='$module_id'";
             return $this->db_fetch_one($sql);
         }
     }
