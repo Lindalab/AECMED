b01f33f420a3d01af5ab667787fd4111def43bcc
Eric Gadzi <78027399+Eric-Gadzi@users.noreply.github.com>
2022-12-25 16:54:41 +0000

    graph done

--- javascript/charts.js
+++ javascript/charts.js
@@ -1,50 +1,76 @@
- function bar() {
-    CanvasJS.addColorSet("ashesiColorShades",
-                [//colorSet Array
-                "#2b0202",
-                "#d60b0b",
-				"#960808",
-                "#ef9d9d",
-                "#da2323",
-				"#ab0909",
-                "#de3c3c",
-                "#e25454",
-                "#e66d6d",
-				"#eb8585",
-				"#f3b6b6",
-				"#f7cece",
-				"#fbe7e7",
-				"#400303",
-				"#560404",
-				"#6b0606",             
-                ]);
- 
-var chart = new CanvasJS.Chart("chartContainer", {
-    colorSet: "ashesiColorShades",
-    interactivityEnabled: false,
-	title: {
-		text: "Push-ups Over a Week"
-	},
-	axisY: {
-		title: "Number of Push-ups",
-		minimum: 0,
-	},
-	data: [{
-		type: "column",
-        indexLabel: "{y}",
-        //color: ["#D60B0B","#950808","#36454F"],
-		yValueFormatString: "#,###.##",
-		indexLabelPlacement: "inside",
-		indexLabelFontColor: "black",
-		indexLabelFontSize: 18,
-		indexLabelFontWeight: "bolder",
-		showInLegend: true,
-		legendText: "{label}",
+{
+  /* <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script> */
+}
 
-        dataPoints: dataPoints,
-	}]
-});
-chart.render();
+var canvasJS = document.createElement("script");
+canvasJS.setAttribute(
+  "src",
+  "https://canvasjs.com/assets/script/canvasjs.min.js"
+);
+document.head.appendChild(canvasJS);
 
+function draw(
+  graph_type,
+  container_name,
+  graph_title,
+  ylabel,
+  xlabel,
+  dataPoints
+) {
+  window.onload = function () {
+    CanvasJS.addColorSet("greenShades", [
+      //colorSet Array
 
+      "#d60b0b",
+      "#960808",
+      // "#da2323",
+      "#ab0909",
+      "#de3c3c",
+      "#e25454",
+      "#e66d6d",
+      "#eb8585",
+      "#ef9d9d",
+      "#f3b6b6",
+      "#f7cece",
+      "#fbe7e7",
+      "#2b0202",
+      "#400303",
+      "#560404",
+      "#6b0606",
+    ]);
+
+    var chart = new CanvasJS.Chart("" + container_name, {
+      colorSet: "greenShades",
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: "" + graph_title,
+      },
+      axisY: {
+        title: "" + ylabel,
+        minimum: 0,
+      },
+      axisX: {
+        title: "" + xlabel,
+        // minimum: 0,
+      },
+      data: [
+        {
+          type: "" + graph_type,
+          indexLabel: "{y}",
+          //color: ["#D60B0B","#950808","#36454F"],
+          yValueFormatString: "#,###.##",
+          indexLabelPlacement: "inside",
+          indexLabelFontColor: "black",
+          indexLabelFontSize: 18,
+          indexLabelFontWeight: "bolder",
+          showInLegend: true,
+          legendText: "{label}",
+
+          dataPoints: dataPoints,
+        },
+      ],
+    });
+    chart.render();
+  };
 }
--- javascript/graph.php
+++ javascript/graph.php
@@ -26,63 +26,12 @@ foreach($res2 as $item){
 <!DOCTYPE HTML>
 <html>
 <head>
+<script src="charts.js"></script>
 <script>
-window.onload = function draw() {
-    CanvasJS.addColorSet("greenShades",
-                [//colorSet Array
-
-                "#d60b0b",
-				"#960808",
-                // "#da2323",
-				"#ab0909",
-                "#de3c3c",
-                "#e25454",
-                "#e66d6d",
-				"#eb8585",
-				"#ef9d9d",
-				"#f3b6b6",
-				"#f7cece",
-				"#fbe7e7",
-				"#2b0202",
-				"#400303",
-				"#560404",
-				"#6b0606",             
-                ]);
- 
-var chart = new CanvasJS.Chart("chartContainer", {
-    colorSet: "greenShades",
-	animationEnabled: true,
-	exportEnabled: true,
-	title: {
-		text: "Push-ups Over a Week"
-	},
-	axisY: {
-		title: "Number of Push-ups",
-		minimum: 0,
-	},
-	data: [{
-		type: "pie",
-        indexLabel: "{y}",
-        //color: ["#D60B0B","#950808","#36454F"],
-		yValueFormatString: "#,###.##",
-		indexLabelPlacement: "inside",
-		indexLabelFontColor: "black",
-		indexLabelFontSize: 18,
-		indexLabelFontWeight: "bolder",
-		showInLegend: true,
-		legendText: "{label}",
-
-        dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-	}]
-});
-chart.render();
-
-
-}
+draw("pie","Container","fun", "ylabel", "xlabel", <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>);
 </script>
 </head>
 <body>
-<div id="chartContainer" style="height: 370px; width: 100%; display: inline;"></div>
-<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+<div id="Container" style="height: 370px; width: 100%; display: inline;"></div>
 </body>
 </html>
