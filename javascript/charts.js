706e9820aa3ebb8827719e18a20b0f7cc6d4fdca
Eric Gadzi <78027399+Eric-Gadzi@users.noreply.github.com>
2022-12-25 21:22:53 +0000

    pie done

--- javascript/charts.js
+++ javascript/charts.js
@@ -1,15 +1,84 @@
-{
-  /* <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script> */
-}
-
-var canvasJS = document.createElement("script");
-canvasJS.setAttribute(
-  "src",
-  "https://canvasjs.com/assets/script/canvasjs.min.js"
-);
-document.head.appendChild(canvasJS);
+// var canvasJS = document.createElement("script");
+// canvasJS.setAttribute(
+//   "src",
+//   "https://canvasjs.com/assets/script/canvasjs.min.js"
+// );
+// document.head.appendChild(canvasJS);
 
 function draw(
+  graph_type,
+  container_name,
+  graph_title,
+  ylabel,
+  xlabel,
+  dataPoints
+) {
+
+
+  CanvasJS.addColorSet("greenShades", [
+    //colorSet Array
+	// "#700505",
+	// "#9b5050",
+    "#d60b0b",
+    // "#960808",
+    // // "#da2323",
+    // "#ab0909",
+	"#fbe7e7",
+	// "#400303",
+    // "#de3c3c",
+    // "#e25454",
+    // "#e66d6d",
+    "#eb8585",
+    // "#ef9d9d",
+    // "#f3b6b6",
+    "#f7cece",
+    "#fbe7e7",
+    "#2b0202",
+    "#400303",
+    "#560404",
+    "#6b0606",
+  ]);
+
+  var chart = new CanvasJS.Chart("" + container_name, {
+    colorSet: "greenShades",
+    animationEnabled: true,
+    exportEnabled: true,
+    title: {
+      text: "" + graph_title,
+	  fontsize: 10,
+    },
+    axisY: {
+      title: "" + ylabel,
+      minimum: 0,
+    },
+    axisX: {
+      title: "" + xlabel,
+      // minimum: 0,
+    },
+    data: [
+      {
+        type: "" + graph_type,
+        indexLabel: "{y}",
+        //color: ["#D60B0B","#950808","#36454F"],
+        yValueFormatString: "#,###.##",
+        indexLabelPlacement: "inside",
+        indexLabelFontColor: "black",
+        indexLabelFontSize: 18,
+        indexLabelFontWeight: "light",
+        showInLegend: true,
+        legendText: "{label}",
+
+        dataPoints: dataPoints,
+      },
+    ],
+  });
+  chart.render();
+}
+
+
+
+
+function draw2(
   graph_type,
   container_name,
   graph_title,
@@ -39,7 +108,7 @@ function draw(
       "#6b0606",
     ]);
 
-    var chart = new CanvasJS.Chart("" + container_name, {
+    var chart2 = new CanvasJS.Chart("" + container_name, {
       colorSet: "greenShades",
       animationEnabled: true,
       exportEnabled: true,
@@ -71,6 +140,6 @@ function draw(
         },
       ],
     });
-    chart.render();
+    chart2.render();
   };
 }
--- javascript/graph.php
+++ javascript/graph.php
@@ -1,37 +1,40 @@
 <?php
- 
-$conn = mysqli_connect('localhost',"root","","shoppn");
+
+$conn = mysqli_connect('localhost', "root", "", "shoppn");
 $sql = "SELECT * FROM `products`";
-$res = mysqli_query($conn,$sql);
+$res = mysqli_query($conn, $sql);
 $res2 = mysqli_fetch_all($res, MYSQLI_ASSOC);
-print_r($res2);
+// print_r($res2);
 
-$dataPoints = array(
-	// array("y" => 25, "label" => "Sunday"),
-	// array("y" => 15, "label" => "Monday"),
-	// array("y" => 25, "label" => "Tuesday"),
-	// array("y" => 5, "label" => "Wednesday"),
-	// array("y" => 10, "label" => "Thursday"),
-	// array("y" => 0, "label" => "Friday"),
-	// array("y" => 20, "label" => "Saturday")
-);
+$dataPoints = array();
 
-foreach($res2 as $item){
+foreach ($res2 as $item) {
 	array_push($dataPoints, array("y" => $item['product_price'], "label" => $item['product_title']));
 }
 
 
- 
+
 ?>
+
+
 <!DOCTYPE HTML>
 <html>
+
 <head>
-<script src="charts.js"></script>
-<script>
-draw("pie","Container","fun", "ylabel", "xlabel", <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>);
-</script>
+	
+	<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+	<script src="charts.js"></script>
+	
 </head>
+
 <body>
-<div id="Container" style="height: 370px; width: 100%; display: inline;"></div>
+	<div id="container" style="height: 370px; width: 50%;"></div>
+	<div id="container2" style="height: 370px; width: 50%;"></div>
+	<script>
+			draw("pie", "container", "fun", "ylabel", "xlabel", <?php  echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>);
+
+			draw("pie", "container2", "fun2", "ylabel", "xlabel", <?php  echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>);
+	</script>
 </body>
+
 </html>
