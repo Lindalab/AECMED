2c86d8eff8c045d07966de0cfc2b2e6ce8c0be00
Lindalab <linda.arthur@ashesi.edu.gh>
2022-12-25 14:37:38 +0000

    sdhjagi

--- view/AVI businesses.php
+++ view/AVI businesses.php
@@ -7,7 +7,7 @@
     <title>AEC - Ashesi Venture Incubator</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-<<<<<<<< HEAD:view/AVI businesses.php
+
     <link rel="stylesheet" href="./css/main.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer" async></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous" defer></script>
@@ -19,7 +19,6 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous" defer></script>
     <script src="./../../js/pagination.js" async defer></script>
->>>>>>>> a0ab27656da31ead928913ebe1eb320439586709:view/AVI/AVI businesses.php
 </head>
 <body>
     <nav>
@@ -27,7 +26,7 @@
             <h1>AEC</h1>
         </header>
         <ul id="list">
-<<<<<<<< HEAD:view/AVI businesses.php
+
             <a href="Summary.php" class="links">
                 <li class="link">
                     <img class="nav-logo" src="./assets/summary-icon.svg" alt="Design Lab Icon">
